64d23a11792939b9e42004fd0eac2245e33f071b
ArtemFuhr <artem.ips@gmail.com>
2022-06-01 18:30:39 +0500

    update

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
@@ -16,6 +16,5 @@ public class Calculator {
     Predicate<Integer> isPositive = x -> x > 0;
 
     Consumer<Integer> println = System.out ::println;
-
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,8 @@ public class Main {
 
     int a = calc.plus.apply(1, 2);
     int b = calc.minus.apply(1, 1);
-    int c = calc.devide.apply(a, b);
-    //ошибка из-за деления на ноль
-        //необходима проверка в devide: если b != 0: выполняет
-        //иначе b==0 то возвращает 0
+    int c = calc.devide.apply(a, b); //3/0
+
 
     calc.println.accept(c);
     }
